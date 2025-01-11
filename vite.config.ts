4d4b678ad9de2b43f296293b97b69a9158f691fe
Felipe Macedo <99462868+Zuk3s@users.noreply.github.com>
2025-01-11 01:18:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/coffee-listening.github.io/", // Substitua pelo nome do seu repositório
 });
